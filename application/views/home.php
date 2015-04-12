55c3a4a39eb620b23f90025df1b7dde958d4d529
Priya Tyagi <priya@leiansis.com>
2015-04-12 08:22:10 +0530

    children's record page made responsive

--- application/views/home.php
+++ application/views/home.php
@@ -18,12 +18,47 @@
         #nav-bar
         {
             padding:1%;
+            background:white;
+        }
+        
+        body
+        {
+            background:#FFD5B2;
         }
         
         #out
         {
             text-align:right;
             font-weight:bold;
+        }
+        
+        .btn-primary
+        {
+            border:0;
+            color:#AB8F77;
+            background:transparent; 
+            transition: background 0.4s, color 0.4s;
+        }
+        
+        .btn-primary:hover
+        {
+            color:white;
+            background:#AB8F77;            
+        }
+        
+        .btn-default
+        {
+            border-color:#AB3838;
+            color:white;
+            background:#AB3838; 
+            transition: background 0.4s, color 0.4s;
+        }
+        
+        .btn-default:hover
+        {
+            border-color:#AB3838;
+            color:#AB3838;
+            background:white;            
         }
         
         #out a 
@@ -39,18 +74,19 @@
         
         #select-element
         {
-            margin-top:10%;
+            margin-top:6%;
         }
-        
+
         #child-details-table
         {
             margin-top:10%;
+            width:80%;
         }
 
         th
         {
             height: 54px;
-            width: 11%;
+            width: 10%;
             text-align:center;
         }
        
@@ -62,9 +98,8 @@
                     th {
   background: #917965;
   height: 54px;
-  width: 16%;
   font-weight: lighter;
-font-size:18px;
+  font-size:18px;
   text-shadow: 0 1px 0 #6E5C4C;
   color: white;
   border: 1px solid #6E5C4C;
@@ -101,6 +136,90 @@ td {
 td:last-child {
   border-right: 0px;
 }
+        
+    #record_header
+        {
+            text-transform:uppercase;
+            font-size:36px;
+            text-align:center;
+            text-decoration:underline;
+            margin-top:8%;
+        }
+        
+        @media screen and (max-width: 800px)
+        {
+            #record_header
+            {
+                font-size: 30px;
+            }
+
+            .btn-default
+            {
+                margin-top:2%;
+            }
+            
+            th
+            {
+                font-size: 14px;
+            }
+            
+            td
+            {
+                font-size: 12px;
+            }
+        }
+        
+        @media screen and (max-width: 460px)
+        {
+            #out a
+            {
+                font-size:14px;
+            }
+            
+            th
+            {
+                font-size: 10px;
+                height: 30px;
+            }
+            
+            td
+            {
+                font-size: 10px;
+                padding:8px;
+            }
+            
+            #child-details-table
+            {
+                width:100%;
+            }
+            
+        }
+        
+         @media screen and (max-width: 350px)
+        {
+            #record_header
+            {
+                font-size: 24px;
+            }
+            
+            th
+            {
+                font-size: 8px;
+                height: 24px;
+            }
+            
+            td
+            {
+                font-size: 6px;
+                padding:6px;
+            }
+            
+            #child-details-table
+            {
+                width:100%;
+            }
+            
+        }
     </style>  
       
     </head>
@@ -109,13 +228,14 @@ td:last-child {
    
         <div id='nav-bar'>
             <div id='logout'>
-                <div id='out'><a class="btn btn-danger" href="#">Logout <span class="glyphicon glyphicon-log-out"></span></a></div>
+                <div id='out'><a class="btn btn-primary" href="#">Logout <span class="glyphicon glyphicon-log-out"></span></a></div>
             </div>
         </div>
-        <div class="container-fluid">
+        <div class="container-fluid" style="padding-bottom:6%;">
             <div class="row">
                 <div class="col-sm-1"></div>
                 <div class="col-sm-9">
+                    <h1 id='record_header'>children's records</h1>
                     <form method="post" action="<?php echo url::base() ?>index.php/home/" class="form">
                         <div class="form-group" id='select-element'>
                             <label class="col-sm-3" for='select-place'>Display details based on:</label>
@@ -128,7 +248,7 @@ td:last-child {
                                  </select>
                             </div>
                             <div class="col-sm-2">
-                                <button class="btn btn-default" type = "submit">Search record</button>
+                                <button class="btn btn-default" type = "submit">Display records</button>
                             </div>
                         </div>
                     </form>
@@ -136,8 +256,9 @@ td:last-child {
                 <div class="col-sm-2"></div>
             </div>
             <div class="row">
-                <div class="col-sm-2"></div>
-                <div class="col-sm-8">
+<!--                <div class="col-sm-1"></div>-->
+<!--                <div class="col-sm-12">-->
+                    <center>
                     <div id='child-details-table'>
                     <?php 
     
@@ -173,7 +294,8 @@ td:last-child {
 }
                     ?>
                     </div>
-                </div>
+                    </center>
+<!--                </div>-->
             </div>
         </div>
          <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
